f665f8e525be23f14fe716c6d3071156bea03233
SHREYANSH SINGH <sshreyansh962@gmail.com>
2024-10-23 18:52:22 +0530

    upload get route and form listing done

---
+++ routes/upload.js
@@ -0,0 +1,10 @@
+// const express = require('express');
+// const path = require('path');
+// const router = express.Router();
+
+// // Serve the upload form when '/upload' is accessed
+// router.get('/upload', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../views/upload.html'));  // Adjust the path if necessary
+// });
+
+// module.exports = router;
